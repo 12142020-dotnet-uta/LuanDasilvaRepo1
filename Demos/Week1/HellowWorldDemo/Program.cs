c27f9b9bc56eb764af159d94590e95562a15da40
ldasilva08 <luandasilva468@gmail.com>
2020-12-16 11:56:21 -0500

    uptoday3

---
+++ Demos/Week1/HellowWorldDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HellowWorldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a string!");
+
+            
+
+            string response =Console.ReadLine();
+
+            Console.WriteLine($"Your response is  {response}");
+        }
+    }
+}

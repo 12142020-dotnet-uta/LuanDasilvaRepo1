9bc6a2c61314762538347a2574b29c5528c1a10f
ldasilva08 <luandasilva468@gmail.com>
2020-12-21 02:11:47 -0500

    Last adds

--- Demos/Week1/RPSGameNoDB/rps/PlayerBase.cs
+++ Demos/Week1/RPSGameNoDB/rps/PlayerBase.cs
@@ -3,7 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace rps
-{
+
+//idea is for this to be singleton class
+
+
+
+/// 
+/// public sealed class Singleton
+    // {
+    // private static readonly Lazy<Singleton>
+    //     lazy =
+    //     new Lazy<Singleton>
+    //         (() => new Singleton());
+
+    //         public static Singleton Instance { get { return lazy.Value; } }
+
+    //         private Singleton()
+    //         {
+    //         }
+    //         }
+///{
+{    
     public class PlayerBase
     {
     static private List<Player> playerBase;
